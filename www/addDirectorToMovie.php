e0cf0f89213fbb8406a243d62cdbe37176896c8e
Mitchell Binning <mitchellbinning@gmail.com>
2015-10-29 22:37:03 -0700

    FUCK  YOU ADD DIRECTOR TO MOVIE DONE BUT DUPES WONT FUCKING IGNORE FUCK YOU

--- www/addDirectorToMovie.php
+++ www/addDirectorToMovie.php
@@ -1,6 +1,29 @@
-<?php>
+<?php
+	// Establish database connection and do basic error handling
+	$db_connection = mysql_connect("localhost", "cs143", "");
+	if(!$db_connection) {
+		$errmsg = mysql_error($db_connection);
+		print "Connection failed: $errmsg <br />";
+		exit(1);
+	}
 
-<?>
+	// Choose database
+	mysql_select_db("CS143", $db_connection);
+	
+	// Get All Movies
+	$get_movies_sql = "SELECT id, title, year
+					  FROM Movie";
+	// Get all Directors
+	$get_directors_sql = "SELECT id, last, first, dob
+						  FROM Director";
+	
+	// Run Queries
+	$all_movies = mysql_query($get_movies_sql, $db_connection);
+	$all_directors = mysql_query($get_directors_sql, $db_connection);
+	
+	mysql_close(databaseConnection);
+	
+?>
 
 <link rel="stylesheet" href="bootstrap.css" type="text/css">
 <link rel="stylesheet" href="style.css" type="text/css">
@@ -11,6 +34,67 @@
 <a class="btn btn-success btn-lg center" href="./index.php" role="button">Go home</a>
 
 <h1 class="center"> Add director-to-movie relation </h1>
+<br/>
+<h2>
+	<form action="addDirectorToMovie.php" method="POST">
+		Movie: <br/>
+		<select name="movie">
+			<?php
+				while($row = mysql_fetch_row($all_movies)){
+					echo "<option value=$row[0]>" . $row[1] . "</option>"; 
+				}
+			?>
+		</select>
+		<br/>
+		Director: <br/>
+		<select name="director">
+			<?php
+			while($row = mysql_fetch_row($all_directors)){
+					echo "<option value=$row[0]>" . $row[1] . ", " . $row[2] . "</option>"; 
+				}
+			?>
+		</select>
+		<br/> <br/>
+		<input type = "submit" name="submit">
+	</form>
+</h2>
 
 </body>
 </html>
+
+<?PHP
+	// Establish database connection and do basic error handling
+	$db_connection = mysql_connect("localhost", "cs143", "");
+	if(!$db_connection) {
+		$errmsg = mysql_error($db_connection);
+		print "Connection failed: $errmsg <br />";
+		exit(1);
+	}
+
+	// Choose database
+	mysql_select_db("CS143", $db_connection);
+	
+	// Submit clicked:
+	if (isset($_POST["submit"])) {
+		$movie_id = $_POST[movie];
+		$director_id = $_POST[director];
+		echo $movie_id;
+		echo '<br/>';
+		echo $director_id;
+		echo '<br/>';
+			
+			// TODO: WHAT THE MOTHER FUCKING FUCK IS WITH THE FUCKING DUPES I SWEAR TO GOD I COULD KILL A FUCKING RHNIOCEROUS MONSTER GIRAFFE FUCK I AM SO FUCKING PISSED FUCK YOU YOU FUCKING DICKWAD ASS FUCK I WILL FUCKING KILL MYSELF :(
+		$add_sql = "INSERT IGNORE INTO MovieDirector (mid, did)
+					VALUES ($movie_id, $director_id)";
+								
+		if (mysql_query($add_sql, $db_connection)){
+			echo "added director to movie ";
+		} 
+		else { 
+			echo "Failed "; 
+		}
+		
+	}
+	
+	mysql_close(databaseConnection);
+?>
