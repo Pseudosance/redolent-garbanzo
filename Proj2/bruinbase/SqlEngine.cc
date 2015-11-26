23daf14986ef70c6cb9f8e77cf2b9aed2d872a6c
Tremaine Eto <tremaineeto@ucla.edu>
2015-11-26 01:13:52 -0800

    implemented the index part of load()

--- Proj2/bruinbase/SqlEngine.cc
+++ Proj2/bruinbase/SqlEngine.cc
@@ -14,177 +14,185 @@
 #include <fstream>
 #include "Bruinbase.h"
 #include "SqlEngine.h"
+#include "BTreeIndex.h"     // added for part d
 
 using namespace std;
 
-// external functions and variables for load file and sql command parsing 
+// external functions and variables for load file and sql command parsing
 extern FILE* sqlin;
 int sqlparse(void);
 
 
 RC SqlEngine::run(FILE* commandline)
 {
-  fprintf(stdout, "Bruinbase> ");
-
-  // set the command line input and start parsing user input
-  sqlin = commandline;
-  sqlparse();  // sqlparse() is defined in SqlParser.tab.c generated from
-               // SqlParser.y by bison (bison is GNU equivalent of yacc)
-
-  return 0;
+    fprintf(stdout, "Bruinbase> ");
+    
+    // set the command line input and start parsing user input
+    sqlin = commandline;
+    sqlparse();  // sqlparse() is defined in SqlParser.tab.c generated from
+    // SqlParser.y by bison (bison is GNU equivalent of yacc)
+    
+    return 0;
 }
 
 RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
 {
-  RecordFile rf;   // RecordFile containing the table
-  RecordId   rid;  // record cursor for table scanning
-
-  RC     rc;
-  int    key;     
-  string value;
-  int    count;
-  int    diff;
-
-  // open the table file
-  if ((rc = rf.open(table + ".tbl", 'r')) < 0) {
-    fprintf(stderr, "Error: table %s does not exist\n", table.c_str());
-    return rc;
-  }
-
-  // scan the table file from the beginning
-  rid.pid = rid.sid = 0;
-  count = 0;
-  while (rid < rf.endRid()) {
-    // read the tuple
-    if ((rc = rf.read(rid, key, value)) < 0) {
-      fprintf(stderr, "Error: while reading a tuple from table %s\n", table.c_str());
-      goto exit_select;
+    RecordFile rf;   // RecordFile containing the table
+    RecordId   rid;  // record cursor for table scanning
+    
+    RC     rc;
+    int    key;
+    string value;
+    int    count;
+    int    diff;
+    
+    // open the table file
+    if ((rc = rf.open(table + ".tbl", 'r')) < 0) {
+        fprintf(stderr, "Error: table %s does not exist\n", table.c_str());
+        return rc;
     }
-
-    // check the conditions on the tuple
-    for (unsigned i = 0; i < cond.size(); i++) {
-      // compute the difference between the tuple value and the condition value
-      switch (cond[i].attr) {
-      case 1:
-	diff = key - atoi(cond[i].value);
-	break;
-      case 2:
-	diff = strcmp(value.c_str(), cond[i].value);
-	break;
-      }
-
-      // skip the tuple if any condition is not met
-      switch (cond[i].comp) {
-      case SelCond::EQ:
-	if (diff != 0) goto next_tuple;
-	break;
-      case SelCond::NE:
-	if (diff == 0) goto next_tuple;
-	break;
-      case SelCond::GT:
-	if (diff <= 0) goto next_tuple;
-	break;
-      case SelCond::LT:
-	if (diff >= 0) goto next_tuple;
-	break;
-      case SelCond::GE:
-	if (diff < 0) goto next_tuple;
-	break;
-      case SelCond::LE:
-	if (diff > 0) goto next_tuple;
-	break;
-      }
-    }
-
-    // the condition is met for the tuple. 
-    // increase matching tuple counter
-    count++;
-
-    // print the tuple 
-    switch (attr) {
-    case 1:  // SELECT key
-      fprintf(stdout, "%d\n", key);
-      break;
-    case 2:  // SELECT value
-      fprintf(stdout, "%s\n", value.c_str());
-      break;
-    case 3:  // SELECT *
-      fprintf(stdout, "%d '%s'\n", key, value.c_str());
-      break;
-    }
-
-    // move to the next tuple
+    
+    // scan the table file from the beginning
+    rid.pid = rid.sid = 0;
+    count = 0;
+    while (rid < rf.endRid()) {
+        // read the tuple
+        if ((rc = rf.read(rid, key, value)) < 0) {
+            fprintf(stderr, "Error: while reading a tuple from table %s\n", table.c_str());
+            goto exit_select;
+        }
+        
+        // check the conditions on the tuple
+        for (unsigned i = 0; i < cond.size(); i++) {
+            // compute the difference between the tuple value and the condition value
+            switch (cond[i].attr) {
+                case 1:
+                    diff = key - atoi(cond[i].value);
+                    break;
+                case 2:
+                    diff = strcmp(value.c_str(), cond[i].value);
+                    break;
+            }
+            
+            // skip the tuple if any condition is not met
+            switch (cond[i].comp) {
+                case SelCond::EQ:
+                    if (diff != 0) goto next_tuple;
+                    break;
+                case SelCond::NE:
+                    if (diff == 0) goto next_tuple;
+                    break;
+                case SelCond::GT:
+                    if (diff <= 0) goto next_tuple;
+                    break;
+                case SelCond::LT:
+                    if (diff >= 0) goto next_tuple;
+                    break;
+                case SelCond::GE:
+                    if (diff < 0) goto next_tuple;
+                    break;
+                case SelCond::LE:
+                    if (diff > 0) goto next_tuple;
+                    break;
+            }
+        }
+        
+        // the condition is met for the tuple.
+        // increase matching tuple counter
+        count++;
+        
+        // print the tuple
+        switch (attr) {
+            case 1:  // SELECT key
+                fprintf(stdout, "%d\n", key);
+                break;
+            case 2:  // SELECT value
+                fprintf(stdout, "%s\n", value.c_str());
+                break;
+            case 3:  // SELECT *
+                fprintf(stdout, "%d '%s'\n", key, value.c_str());
+                break;
+        }
+        
+        // move to the next tuple
     next_tuple:
-    ++rid;
-  }
-
-  // print matching tuple count if "select count(*)"
-  if (attr == 4) {
-    fprintf(stdout, "%d\n", count);
-  }
-  rc = 0;
-
-  // close the table file and return
-  exit_select:
-  rf.close();
-  return rc;
+        ++rid;
+    }
+    
+    // print matching tuple count if "select count(*)"
+    if (attr == 4) {
+        fprintf(stdout, "%d\n", count);
+    }
+    rc = 0;
+    
+    // close the table file and return
+exit_select:
+    rf.close();
+    return rc;
 }
 
 /*
-when the user issues the command "LOAD movie FROM 'movieData.del'", 
-you should create a RecordFile named movie.tbl (in the current working directory) and store all tuples in the file. 
-If the file already exists, the LOAD command should append all records in the load file to the end of the table. 
-Roughly, your implementation of the load function should open the input loadfile and the RecordFile,
-parse each line of the loadfile to read a tuple (possibly using SqlEngine::parseLoadLine()) and insert the tuple to the RecordFile.
-*/
+ when the user issues the command "LOAD movie FROM 'movieData.del'",
+ you should create a RecordFile named movie.tbl (in the current working directory) and store all tuples in the file.
+ If the file already exists, the LOAD command should append all records in the load file to the end of the table.
+ Roughly, your implementation of the load function should open the input loadfile and the RecordFile,
+ parse each line of the loadfile to read a tuple (possibly using SqlEngine::parseLoadLine()) and insert the tuple to the RecordFile.
+ */
 RC SqlEngine::load(const string& table, const string& loadfile, bool index)
 {
-  /* your code here */
-  // Copied from the select function
-  printf("Entered Load");
-  RecordFile rf;   // record for the table
-  RecordId   rid;  // record cursor for table scanning
-
-  RC     rc;
-  int    key;     
-  string value;
-  
-  // open the table file (Copied from select function, but with write attribute instead of read
-  if ((rc = rf.open(table + ".tbl", 'w')) < 0) { // Create a record file named movie.tbl
-    fprintf(stderr, "Error: Failed to create or open table %s \n", table.c_str());
-    return rc;
-  }
-  
-  if(index)
-  {
+    /* your code here */
+    // Copied from the select function
+    printf("Entered Load");
+    RecordFile rf;   // record for the table
+    RecordId   rid;  // record cursor for table scanning
     
-    // Do indexy shit later
+    RC     rc;
+    int    key;
+    string value;
     
-  }
-  else{
-    // Get end of records 
-    rid = rf.endRid();
-    // open file stream 
-    fstream loading (loadfile.c_str());
-    if(loading.is_open()){
-      string line; 
-      while(getline(loading, line))
-      {
-        // Append to table
-        parseLoadLine(line, key, value);
-        rc = rf.append(key, value, rid);
-      }
-        loading.close();
+    // open the table file (Copied from select function, but with write attribute instead of read
+    if ((rc = rf.open(table + ".tbl", 'w')) < 0) { // Create a record file named movie.tbl
+        fprintf(stderr, "Error: Failed to create or open table %s \n", table.c_str());
+        return rc;
+    }
+    
+    BTreeIndex bTreeIndex;
+    if(index)
+    {
+        // creates the corresponding B+tree index on the key column of the table
+        // index file should be named 'tblname.idx' where tblname is the name of the table, and created in the current working directory
+        // for every tuple inserted into the table, you obtain RecordId of the inserted tuple, and insert a corresponding (key, RecordId) pair to the B+tree of the table
+        
+        if ((rc = bTreeIndex.open(table + ".idx", 'w') != 0)) {     // index failed to open
+            fprintf(stderr, "Error: Failed to open index \n");
+            return rc;
+        }
+        
     }
     else{
-          fprintf(stderr, "Error: Failed to open load file %s \n", loadfile.c_str());
-          rc =  RC_FILE_OPEN_FAILED;
+        // Get end of records
+        rid = rf.endRid();
+        // open file stream
+        fstream loading (loadfile.c_str());
+        if(loading.is_open()){
+            string line;
+            while(getline(loading, line))
+            {
+                // Append to table
+                parseLoadLine(line, key, value);
+                rc = rf.append(key, value, rid);
+            }
+            loading.close();
+        }
+        else{
+            fprintf(stderr, "Error: Failed to open load file %s \n", loadfile.c_str());
+            rc =  RC_FILE_OPEN_FAILED;
+        }
     }
-  }
- 
-  rf.close();
-  
-  return rc;
+    
+    rf.close();
+    
+    return rc;
 }
 
 RC SqlEngine::parseLoadLine(const string& line, int& key, string& value)
@@ -196,14 +204,14 @@ RC SqlEngine::parseLoadLine(const string& line, int& key, string& value)
     // ignore beginning white spaces
     c = *(s = line.c_str());
     while (c == ' ' || c == '\t') { c = *++s; }
-
+    
     // get the integer key value
     key = atoi(s);
-
+    
     // look for comma
     s = strchr(s, ',');
     if (s == NULL) { return RC_INVALID_FILE_FORMAT; }
-
+    
     // ignore white spaces
     do { c = *++s; } while (c == ' ' || c == '\t');
     
@@ -212,18 +220,18 @@ RC SqlEngine::parseLoadLine(const string& line, int& key, string& value)
         value.erase();
         return 0;
     }
-
+    
     // is the value field delimited by ' or "?
     if (c == '\'' || c == '"') {
         s++;
     } else {
         c = '\n';
     }
-
+    
     // get the value string
     value.assign(s);
     loc = value.find(c, 0);
     if (loc != string::npos) { value.erase(loc); }
-
+    
     return 0;
 }
