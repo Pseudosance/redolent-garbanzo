541d282e559525d958394392c5addf5ba7ad27d5
Tremaine Eto <tremaineeto@ucla.edu>
2015-12-02 17:04:29 -0800

    added P2.zip

--- Proj2/bruinbase/test.sql
+++ Proj2/bruinbase/test.sql
@@ -19,4 +19,3 @@ LOAD xlarge FROM 'xlarge.del' WITH INDEX
 SELECT COUNT(*) FROM xlarge
 SELECT * FROM xlarge WHERE key = 4240
 SELECT * FROM xlarge WHERE key > 400 AND key < 500 AND key > 100 AND key < 4000000
-
